60deeb18a2849d584b1338324b429f923000e311
Cristian <CrisRPia>
2024-11-05 18:42:28 -0300

    charts work

--- charts/src/app/app.config.server.ts
+++ charts/src/app/app.config.server.ts
@@ -1,10 +1,12 @@
 import { mergeApplicationConfig, ApplicationConfig } from '@angular/core';
 import { provideServerRendering } from '@angular/platform-server';
 import { appConfig } from './app.config';
+import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
 
 const serverConfig: ApplicationConfig = {
   providers: [
-    provideServerRendering()
+    provideServerRendering(),
+    provideCharts(withDefaultRegisterables())
   ]
 };
 
--- charts/src/app/app.config.ts
+++ charts/src/app/app.config.ts
@@ -4,12 +4,13 @@ import { provideRouter } from "@angular/router";
 import { routes } from "./app.routes";
 import { provideClientHydration } from "@angular/platform-browser";
 import { provideHttpClient } from "@angular/common/http";
+import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
 
 export const appConfig: ApplicationConfig = {
     providers: [
         provideZoneChangeDetection({ eventCoalescing: true }),
         provideRouter(routes),
         provideClientHydration(),
-        provideHttpClient(),
+        provideHttpClient(), provideCharts(withDefaultRegisterables()),
     ],
 };
